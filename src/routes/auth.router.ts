06bd118f01ea3957448753d04b2d44aa0a9e9f31
khaitbek <khaitbek2005@gmail.com>
2023-07-12 07:57:10 +0500

    Add get users by id functionality

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -6,8 +6,8 @@ import { Router } from "express";
 export const router = Router();
 
 router.get("/users", Controller.getUsers);
-router.get("/student/:id", authMiddleware, Controller.getStudentById);
-router.get("/teacher/:id", authMiddleware, Controller.getTeacherById);
+router.get("/student", authMiddleware, Controller.getStudentById);
+router.get("/teacher", authMiddleware, Controller.getTeacherById);
 router.get("/students", authMiddleware, Controller.getStudents);
 router.get("/teachers", authMiddleware, Controller.getTeachers);
 router.post("/auth/register", authMiddleware, roleMiddleware("ADMIN"), Controller.register);
